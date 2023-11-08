246ce392b1903f12a7985eb7158afdd6113f0d20
dwarkesh2810 <dwarkeshpatel.siliconithub@gmail.com>
2023-11-08 18:43:29 +0530

    change folder structure

--- internal/modules/post/postController/controller.go
+++ internal/modules/post/postController/controller.go
@@ -1,1 +1,13 @@
 package postcontroller
+
+import postservice "crud/internal/modules/post/postService"
+
+type Controllers struct {
+	postService postservice.PostServiceInterface
+}
+
+func New() *Controllers {
+	return &Controllers{
+		postService: postservice.New(),
+	}
+}
